ce0ea540e9e93330178f736b49d272da0cc33e93
Daniel Hough <32570623+hoofay@users.noreply.github.com>
2018-05-13 20:52:38 +0100

    Add files via upload
    conversion output

--- make_numerics.R
+++ make_numerics.R
@@ -1,11 +1,11 @@
 # author: daniel hough
 # filename: make_numerics.R
-# date: 2018-04-13
+# date: 2018-05-13
 # dependencies: dplyr, lazyeval
 
 library(dplyr)
 
-make_numerics <- function(df,col_name,target_name,output = 'rank'){
+make_numerics <- function(df,col_name,target_name,output = 'rank',convert=FALSE){
   ### convert character / factored columns to scaled numerics
   
   mydf <- df
@@ -14,28 +14,41 @@ make_numerics <- function(df,col_name,target_name,output = 'rank'){
   
   convert <- mydf %>% ungroup() %>% group_by_(.dots=groupL) %>%
     summarise_(s = lazyeval::interp(~mean(a,na.rm = TRUE), a = as.name(target_name))) %>% 
-    mutate(r = rank(s)/length(unique(mydf[,col_name]))) %>% arrange(r)
+    mutate(r = (rank(s)-1)/(length(unique(mydf[,col_name]))-1)) %>% arrange(r)
   
-  foo_name_2 <- c(col_name)
-  if(output == 'rank'){
-  foo_2 <- lazyeval::interp(~r)
-  select_names <- c(col_name,'r')
-  mydf <- mydf %>% left_join(select_(convert, .dots = select_names), by = col_name) %>%
-    mutate_(.dots=setNames(list(foo_2),foo_name_2)) %>% select(-r)
-  } else {
-    foo_2 <- lazyeval::interp(~s)
-    select_names <- c(col_name,'s')
-    mydf <- mydf %>% left_join(select_(convert, .dots = select_names), by = col_name) %>%
-      mutate_(.dots=setNames(list(foo_2),foo_name_2)) %>% select(-s)
+  if(convert==TRUE){
+    
+    return(convert)
+  
+    } else {
+    
+    foo_name_2 <- c(col_name)
+    if(output == 'rank'){
+      foo_2 <- lazyeval::interp(~r)
+      select_names <- c(col_name,'r')
+      mydf <- mydf %>% left_join(select_(convert, .dots = select_names), by = col_name) %>%
+        mutate_(.dots=setNames(list(foo_2),foo_name_2)) %>% select(-r)
+    } else {
+      foo_2 <- lazyeval::interp(~s)
+      select_names <- c(col_name,'s')
+      mydf <- mydf %>% left_join(select_(convert, .dots = select_names), by = col_name) %>%
+        mutate_(.dots=setNames(list(foo_2),foo_name_2)) %>% select(-s)
+    }
+    
+    return(mydf)
   }
-  
-  mydf
 }
 
 # example
-md <- data.frame(result=rnorm(7),text_var = c('d','da','d','dan','dan','da','d'))
+md <- data.frame(result=rnorm(8),text_var = c('d','da','d','dan','dan','da','d',NA))
 
 make_numerics(md,'text_var','result','value')
 
 make_numerics(md,'text_var','result','rank')
+
+
+
+
+
+
 
